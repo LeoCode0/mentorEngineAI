5b2ac98350a24ebb7e3a3716a2b85c1b1c6ee18e
Leonardo Espinoza <leoespinozidk@gmail.com>
2024-04-13 14:11:56 -0600

    ft(setup): add linters

---
+++ packages/nextjs-front/.eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 14,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/display-name': 'off',
+  },
+}
